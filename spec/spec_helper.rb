fe18fa8fbada405a961ebd0af15782b9278c6d8c
R. Tyler Croy <tyler@monkeypox.org>
2016-04-15 12:31:55 -0700

    Add some basic fixture dependencies for apache, concat and stdlib
    puppetlabs/concat's dependency range is wrong as of 2.1.0, it uses is_bool()
    which doesn't appear to have been included in puppetlabs/stdlib until 4.2.0 but
    the version range is puppetlabs/stdlib (>= 3.2.0 < 5.0.0)

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,5 +1,19 @@
 require 'puppetlabs_spec_helper/module_spec_helper'
 
-
 ENV['FUTURE_PARSER'] = 'yes'
 ENV['STRICT_VARIABLES'] = 'yes'
+
+FIXTURES_PATH = File.expand_path(File.dirname(__FILE__) + '/fixtures')
+# Set up our $LOAD_PATH to properly include custom provider code from modules
+# in spec/fixtures
+$LOAD_PATH.unshift(*Dir["#{FIXTURES_PATH}/modules/*/lib"])
+
+
+RSpec.configure do |r|
+  r.default_facts = {
+    :osfamily => 'Debian',
+    :lsbdistrelease => '14.04',
+    :operatingsystem => 'Ubuntu',
+    :operatingsystemrelease => '14.04',
+  }
+end
