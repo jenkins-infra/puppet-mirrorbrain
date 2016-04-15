677c74d38d9d8418611125684f5e9b28b083efdd
R. Tyler Croy <tyler@monkeypox.org>
2016-04-15 12:46:24 -0700

    Include the Apache OBS GPG key so we can include packages from their repos safely

--- manifests/debian.pp
+++ manifests/debian.pp
@@ -2,4 +2,10 @@
 # Manage Debian-specific repositories/dependencies
 class mirrorbrain::debian {
   include ::apt
+
+  apt::key { 'apacheobs':
+    id     => 'EDDDC98D96A0F8899AB07C789584A164BD6D129A',
+    server => 'pgp.mit.edu',
+  }
+
 }
--- spec/classes/debian_spec.rb
+++ spec/classes/debian_spec.rb
@@ -3,4 +3,16 @@ require 'spec_helper'
 describe 'mirrorbrain::debian' do
   it { should contain_class('mirrorbrain::debian') }
   it { should contain_class 'apt' }
+
+  context 'apt GPG key' do
+    it 'should install the appropriate key for the Apache repo on OBS' do
+      expect(subject).to contain_apt__key('apacheobs').with({
+        :server => 'pgp.mit.edu',
+        :id => 'EDDDC98D96A0F8899AB07C789584A164BD6D129A',
+      })
+    end
+  end
+
+  context 'an apt repository' do
+  end
 end
