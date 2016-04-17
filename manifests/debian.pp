1fcdc5a704847a3be72abd0415afe90c259afd2a
R. Tyler Croy <tyler@monkeypox.org>
2016-04-17 09:00:11 -0700

    Custom handle the installation of our debian repository since apt::source won't cooperate

--- manifests/debian.pp
+++ manifests/debian.pp
@@ -3,23 +3,32 @@
 class mirrorbrain::debian {
   include ::apt
 
-  $apt_repo = 'apache-obs'
+  $apt_repo = 'apache-mirrorbrain-obs'
 
-  apt::source { $apt_repo:
-    comment  => 'http://mirrorbrain.org/docs/installation/debian/',
-    location => 'http://download.opensuse.org/repositories/home:/agentdero:/mirrorbrain/xUbuntu_14.04',
-    repos    => '/',
-    release  => '',
-    key      => {
-      id     => '77F11CCF1AFBE15388CE93239FAAEC7EA5638A16',
-      server => 'pgp.mit.edu',
-    },
-    notify   => Exec['apt_update'],
+  apt::key { $apt_repo:
+    ensure => present,
+    id     => '77F11CCF1AFBE15388CE93239FAAEC7EA5638A16',
+    server => 'pgp.mit.edu',
+  }
+
+  # Manually injecting an apt repo list file since apt::source doesn't want to
+  # handle our "weird" OBS debian repository layout and on Ubuntu it tries very
+  # hard to add "trusty" or whatever the codename is into the repos
+  #
+  # This repository is also using @rtyler's custom mirrorbrain updates for
+  # later debian type hosts because upstream has yet to enable them
+  file { "/etc/apt/sources.list.d/${apt_repo}.list":
+    ensure  => present,
+    content => 'deb http://download.opensuse.org/repositories/home:/agentdero:/mirrorbrain/xUbuntu_14.04 /',
+    owner   => 'root',
+    group   => 'root',
+    require => Apt::Key[$apt_repo],
+    notify  => Exec['apt_update'],
   }
 
   package { ['mirrorbrain', 'mirrorbrain-tools', 'mirrorbrain-scanner']:
     ensure  => present,
-    require => Apt::Source[$apt_repo],
+    require => File["/etc/apt/sources.list.d/${apt_repo}.list"],
   }
 
   package { ['geoip-bin', 'geoip-database']:
--- spec/classes/debian_spec.rb
+++ spec/classes/debian_spec.rb
@@ -5,14 +5,18 @@ describe 'mirrorbrain::debian' do
   it { should contain_class 'apt' }
 
   context 'an apt repository' do
-    it 'should include the Apache OBS repository' do
-      expect(subject).to contain_apt__source('apache-obs').with({
-        :location => 'http://download.opensuse.org/repositories/home:/agentdero:/mirrorbrain/xUbuntu_14.04',
-        :repos => '/',
-        :key => {
-          'server' => 'pgp.mit.edu',
-          'id' => '77F11CCF1AFBE15388CE93239FAAEC7EA5638A16'
-        },
+    it 'should include our apt::key' do
+      expect(subject).to contain_apt__key('apache-mirrorbrain-obs').with({
+        :server => 'pgp.mit.edu',
+        :id => '77F11CCF1AFBE15388CE93239FAAEC7EA5638A16'
+      })
+    end
+
+    it 'should include the mirrorbrain repository' do
+      expect(subject).to contain_file('/etc/apt/sources.list.d/apache-mirrorbrain-obs.list').with({
+        :content => 'deb http://download.opensuse.org/repositories/home:/agentdero:/mirrorbrain/xUbuntu_14.04 /',
+        :notify => 'Exec[apt_update]',
+
       })
     end
   end
